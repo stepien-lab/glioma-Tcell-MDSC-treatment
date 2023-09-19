bdef0d26bfe2332cfa2f22d60dad02d1ac5e7a8d
Hannah Anderson <hannahanderson@c0709a-s30.ufhpc>
2023-09-19 08:34:46 -0400

    updating slurm scripts

--- GBMbifucationSlurmscript.sh
+++ GBMbifucationSlurmscript.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1                      # Run a single task
 #SBATCH --cpus-per-task=1               # Check QoS/queue to see how many CPUS are available
 #SBATCH --mem-per-cpu=2gb               # Memory per processor - max 2gb for MATLAB
-#SBATCH --time=00:10:00                 # Time limit hrs:min:sec
+#SBATCH --time=00:20:00                 # Time limit hrs:min:sec
 #SBATCH --output=logs/MATLAB_%j.txt     # Output and error log - create "logs" folder first before running SLURM script
 pwd; hostname; date
 
