b9b316db9b88eb39818a958dabe5c074eef03d94
HannahGrace314 <89090482+HannahGrace314@users.noreply.github.com>
2024-04-22 16:38:07 -0400

    Update GBMFuncoptimal.m

--- GBMFuncoptimal.m
+++ GBMFuncoptimal.m
@@ -1,22 +1,28 @@
 function dxdt = GBMFuncoptimal(t,x,p)
-% function dxdt = GBMFuncoptimal(t,x,p)
 
-% This is our model of treatment free tumor-immune dynamics 
-% without the MDSC expansion term, to fit the model used for optimal
-% control.
+% This is our model of treatment-free glioblastoma-immune dynamics 
+% to be used in conjunction with the optimal control code.
+
+% The purpose of this code is to determine system dynamics pre- and
+% post-treatment.
+
+%--------------------------------------------------------------------------
+
 % INPUTS: 
+
 C = x(1);
 T = x(2);
 M = x(3);
-dxdt = zeros(3,1);
 
 % x is 3x1 vector with:
 % C = number of tumor cells
 % T = number of T cells
 % M = number of MDSCs 
 
+%--------------------------------------------------------------------------
 
 %Parameter values for tumor growth:
+
 lambdaC = p(1);
 Cmax = p(2); 
 eta = p(3); 
@@ -25,8 +31,10 @@ eta = p(3);
 %   Cmax = carrying capacity of tumor cells
 %   eta = kill rate of tumor cells by T cells
 
+%--------------------------------------------------------------------------
 
 %Parameter values for T cells activation/suppression:
+
 aT = p(4);
 sT = p(5);
 rho = p(6);
@@ -41,6 +49,7 @@ dT = p(9);
 %   r = inhibition of T cells by MDSCs
 %   dT = death rate of T cells
 
+%--------------------------------------------------------------------------
 
 % Parameter values for MDSCs
 sM = p(10);  
@@ -49,15 +58,12 @@ dM = p(11);
 % sM = stimulation by CCL2/CCL7
 % dM = death rate of MDSCs
 
+%--------------------------------------------------------------------------
 
-%  output:
-%   dxdt(1) = derivative of the tumor cell equation
-%   dxdt(2) = derivative of the T cell equation
-%   dxdt(3) = derivative of the MDSC equation
-
-
-dxdt(1) = lambdaC*C*(1-(C/Cmax))-eta*T*C; 
-%dxdt(2) = (aT+(sT*T*C))-dT*T;
-dxdt(2) = ((aT+(sT*T*C))/(1+(rho*T*(T+eC*C))))...
-  -r*T*M-dT*T;
-dxdt(3)= (sM*C)-dM*M;
+% ODE FUNCTIONS: 
+dC = lambdaC*C*(1-(C/Cmax))-eta*T*C; 
+dT = ((aT+(sT*T*C))/(1+(rho*T*(T+eC*C)))) -r*T*M -dT*T;
+dM = (sM*C)-dM*M;
+    
+% COMBINE RESULTS:
+dxdt = [dC; dT; dM];
