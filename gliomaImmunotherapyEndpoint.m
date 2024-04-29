b670ea3a100f5c57043eab3f5d2717a355d58ee6
HannahGrace314 <89090482+HannahGrace314@users.noreply.github.com>
2024-04-29 10:56:59 -0400

    Update gliomaImmunotherapyEndpoint.m

--- gliomaImmunotherapyEndpoint.m
+++ gliomaImmunotherapyEndpoint.m
@@ -1,4 +1,6 @@
 function output = GliomaImmunotherapyEndpoint(input)
+% For optimal control using GPOPS-II 
+% used in the code GBMoptimalcontrol.m
 
 Cf = input.phase.finalstate(1);
 output.objective = Cf; 
