5c2609136bdc2c9a1a0068765917007438356603
HannahGrace314 <89090482+HannahGrace314@users.noreply.github.com>
2024-04-29 10:50:43 -0400

    Update gliomaImmunotherapyEndpoint.m

--- gliomaImmunotherapyEndpoint.m
+++ gliomaImmunotherapyEndpoint.m
@@ -2,5 +2,3 @@ function output = GliomaImmunotherapyEndpoint(input)
 
 Cf = input.phase.finalstate(1);
 output.objective = Cf; 
-%idk if I should change more, just changed pf to Cf
-%I know it is the objective functional that we're seeking to minimize
