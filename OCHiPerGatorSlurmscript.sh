e1d65e71ab0559961095e421401b2f608ef1528b
HannahGrace314 <89090482+HannahGrace314@users.noreply.github.com>
2024-04-29 14:07:27 -0400

    Update and rename OCHiPerGatorORIGINALSlurmscript.sh to OCHiPerGatorSlurmscript.sh

--- OCHiPerGatorSlurmscript.sh
+++ OCHiPerGatorSlurmscript.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-#SBATCH --job-name=OCHiPerGatorORIGINAL       # Use the same name as the MATLAB program\
+#SBATCH --job-name=OCHiPerGator       # Use the same name as the MATLAB program\
 #SBATCH --mail-type=ALL                 # Mail events (NONE, BEGIN, END, FAIL, ALL)\
 #SBATCH --mail-user=hannahanderson@ufl.edu    # Where to send mail\
 #SBATCH --account=tstepien
 #SBATCH --qos=tstepien-b
 #SBATCH --nodes=1                       # Use one node\
 #SBATCH --ntasks=1                      # Run a single task\
-#SBATCH --cpus-per-task=2              # Check QoS/queue to see how many CPUS are available\
+#SBATCH --cpus-per-task=4              # Check QoS/queue to see how many CPUS are available\
 #SBATCH --mem-per-cpu=2gb               # Memory per processor - max 2gb for MATLAB\
-#SBATCH --time=00:30:00                 # Time limit hrs:min:sec\
+#SBATCH --time=00:30:00                 # Time limit hrs:min:sec\ % takes about 30 hours for 10,000 parameter sets
 #SBATCH --output=logs/MATLAB_%j.txt     # Output and error log - create "logs" folder first before running SLURM script\
 pwd; hostname; date
 
-module load matlab
-./OCHiPerGatorORIGINAL
+module load matlab/2019a
+./OCHiPerGator
 
 date
 exit
